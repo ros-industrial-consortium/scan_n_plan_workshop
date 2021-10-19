1fc840d0684e590b7f30f706e314ba76dde0e588
Josh Langsfeld <josh.langsfeld@gmail.com>
2021-10-18 19:24:41 -0500

    Various fixes for the workshop setup

--- workshop_setup.bash
+++ workshop_setup.bash
@@ -1,34 +1,34 @@
 #!/bin/bash
 
-# Get Taskflow
-sudo add-apt-repository ppa:ros-industrial/ppa
+# Get Taskflow and misc deps
+sudo add-apt-repository -y ppa:ros-industrial/ppa
 sudo apt-get update
-sudo apt-get install taskflow
+sudo apt-get install -y taskflow libompl-dev
 
 # Clone workshop repo
 cd $HOME
 git clone https://github.com/ros-industrial-consortium/roscon_workshop_2021.git workshop/
 
-# Move to and vcstool dep_ws
-cd ~/workshop/dep_ws
-vcs import src/ < src/deps.rosinstall
+# Move to and vcstool deps_ws
+cd ~/workshop/deps_ws
+vcs import --shallow src/ < src/deps.rosinstall
 rosdep install --from-paths src --ignore-src --rosdistro noetic -r -y
 
 # Move to, vcstool, and rosdep ros1_ws
 cd ~/workshop/ros1_ws
-vcs import src/ < src/ros1.rosinstall
+vcs import --shallow src/ < src/ros1.rosinstall
 rosdep install --from-paths src --ignore-src --rosdistro noetic -r -y
 
 # Move to, vcstool, and rosdep ros2_ws
 cd ~/workshop/ros2_ws
-vcs import src/ < src/ros2.rosinstall
+vcs import --shallow src/ < src/ros2.rosinstall
 rosdep install --from-paths src --ignore-src --rosdistro foxy -r -y
 
 # Move to, vcstool, and rosdep bridge_ws
 cd ~/workshop/bridge_ws
-vcs import src/ < src/bridge.rosinstall
+vcs import --shallow src/ < src/bridge.rosinstall
 rosdep install --from-paths src --ignore-src --rosdistro foxy -r -y
 
-# Build dep_ws
-cd ~/workshop/dep_ws
+# Build deps_ws
+cd ~/workshop/deps_ws
 colcon build --merge-install
