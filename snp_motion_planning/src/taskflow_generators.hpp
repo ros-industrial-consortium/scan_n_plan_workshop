1bc82ff1af08cf2e81ce844100b4df10eada02b5
Michael Ripperger <michael.ripperger@swri.org>
2022-05-03 17:02:42 -0500

    Added input and seed checks to custom taskflows

--- snp_motion_planning/src/taskflow_generators.hpp
+++ snp_motion_planning/src/taskflow_generators.hpp
@@ -3,10 +3,15 @@
 #include <tesseract_motion_planners/trajopt/trajopt_motion_planner.h>
 #include <tesseract_motion_planners/simple/simple_motion_planner.h>
 #include <tesseract_motion_planners/ompl/ompl_motion_planner.h>
-#include <tesseract_process_managers/taskflow_generators/graph_taskflow.h>
+// Task Generators
+#include <tesseract_process_managers/task_generators/check_input_task_generator.h>
+#include <tesseract_process_managers/task_generators/has_seed_task_generator.h>
+#include <tesseract_process_managers/task_generators/seed_min_length_task_generator.h>
 #include <tesseract_process_managers/task_generators/motion_planner_task_generator.h>
 #include <tesseract_process_managers/task_generators/discrete_contact_check_task_generator.h>
 #include <tesseract_process_managers/task_generators/iterative_spline_parameterization_task_generator.h>
+// Taskflow generators
+#include <tesseract_process_managers/taskflow_generators/graph_taskflow.h>
 #include <tesseract_process_managers/taskflow_generators/raster_taskflow.h>
 #include <tesseract_process_managers/core/default_process_planners.h>
 
@@ -22,6 +27,11 @@ tesseract_planning::TaskflowGenerator::UPtr createTransitionTaskflow()
   // Create the graph task flow
   auto graph = std::make_unique<GraphTaskflow>();
 
+  // Input/seed checks
+  auto check_input = graph->addNode(std::make_unique<CheckInputTaskGenerator>(), true);
+  int has_seed = graph->addNode(std::make_unique<HasSeedTaskGenerator>(), true);
+  int seed_min_length = graph->addNode(std::make_unique<SeedMinLengthTaskGenerator>(), true);
+
   // Simple planner with post-collision check
   auto simple_planner = std::make_shared<SimpleMotionPlanner>();
   int simple = graph->addNode(std::make_unique<MotionPlannerTaskGenerator>(simple_planner), true);
@@ -35,6 +45,9 @@ tesseract_planning::TaskflowGenerator::UPtr createTransitionTaskflow()
   // Time parameterization
   int time_param = graph->addNode(std::make_unique<IterativeSplineParameterizationTaskGenerator>(), true);
 
+  graph->addEdges(check_input, { GraphTaskflow::ERROR_NODE, has_seed });
+  graph->addEdges(has_seed, { simple, seed_min_length });
+  graph->addEdges(seed_min_length, { GraphTaskflow::ERROR_NODE, simple_collision });
   graph->addEdges(simple, { GraphTaskflow::ERROR_NODE, simple_collision });
   graph->addEdges(simple_collision, { trajopt, time_param });
   graph->addEdges(trajopt, { GraphTaskflow::ERROR_NODE, trajopt_collision });
@@ -56,6 +69,11 @@ tesseract_planning::TaskflowGenerator::UPtr createFreespaceTaskflow()
   // Create the graph task flow
   auto graph = std::make_unique<GraphTaskflow>();
 
+  // Input/seed checks
+  auto check_input = graph->addNode(std::make_unique<CheckInputTaskGenerator>(), true);
+  int has_seed = graph->addNode(std::make_unique<HasSeedTaskGenerator>(), true);
+  int seed_min_length = graph->addNode(std::make_unique<SeedMinLengthTaskGenerator>(), true);
+
   // Simple planner with post-collision check
   auto simple_planner = std::make_shared<SimpleMotionPlanner>();
   int simple = graph->addNode(std::make_unique<MotionPlannerTaskGenerator>(simple_planner), true);
@@ -76,6 +94,9 @@ tesseract_planning::TaskflowGenerator::UPtr createFreespaceTaskflow()
   // Time parameterization
   int time_param = graph->addNode(std::make_unique<IterativeSplineParameterizationTaskGenerator>(), true);
 
+  graph->addEdges(check_input, { GraphTaskflow::ERROR_NODE, has_seed });
+  graph->addEdges(has_seed, { simple, seed_min_length });
+  graph->addEdges(seed_min_length, { GraphTaskflow::ERROR_NODE, simple_collision });
   graph->addEdges(simple, { GraphTaskflow::ERROR_NODE, simple_collision });
   graph->addEdges(simple_collision, { trajopt, time_param });
   graph->addEdges(trajopt, { ompl, trajopt_collision });
