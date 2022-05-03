efa4940f9c066df6fed1770441ac9c561e7f31bf
Michael Ripperger <michael.ripperger@swri.org>
2022-05-03 17:00:26 -0500

    Added rviz config argument to launch file

--- snp_support/launch/test.launch.py
+++ snp_support/launch/test.launch.py
@@ -1,9 +1,11 @@
 import os
 import yaml
-import xacro
 from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
 from launch_ros.actions import Node
 from ament_index_python import get_package_share_directory
+
 
 def get_package_file(package, file_path):
     """Get the location of a file installed in an ament package"""
@@ -11,21 +13,24 @@ def get_package_file(package, file_path):
     absolute_file_path = os.path.join(package_path, file_path)
     return absolute_file_path
 
+
 def load_file(file_path):
     """Load the contents of a file into a string"""
     try:
         with open(file_path, 'r') as file:
             return file.read()
-    except EnvironmentError: # parent of IOError, OSError *and* WindowsError where available
+    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
         return None
+
 
 def load_yaml(file_path):
     """Load a yaml file into a dictionary"""
     try:
         with open(file_path, 'r') as file:
             return yaml.safe_load(file)
-    except EnvironmentError: # parent of IOError, OSError *and* WindowsError where available
+    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
         return None
+
 
 def run_xacro(xacro_file):
     """Run xacro and output a file in the same directory with the same name, w/o a .xacro suffix"""
@@ -36,7 +41,9 @@ def run_xacro(xacro_file):
     return urdf_file
 
 
-def generate_launch_description():
+def launch_setup(context, *args, **kwargs):
+    rviz_config = LaunchConfiguration('rviz_config').perform(context)
+
     xacro_file = get_package_file('snp_support', 'urdf/workcell.xacro')
     urdf_file = run_xacro(xacro_file)
     srdf_file = get_package_file('snp_support', 'config/workcell.srdf')
@@ -50,9 +57,7 @@ def generate_launch_description():
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
-        parameters=[
-            {'robot_description': robot_description}
-        ]
+        parameters=[{'robot_description': robot_description}]
     )
 
     # joint state publisher so we can tshoot
@@ -70,18 +75,18 @@ def generate_launch_description():
         package='rviz2',
         executable='rviz2',
         output='screen',
-        arguments=['-d' + os.path.join(get_package_share_directory('snp_support'), 'config', 'rviz_base_config.rviz')],
-        parameters=[
-            {
-                'robot_description': robot_description,
-                'robot_description_semantic': robot_description_semantic,
-            }
-        ],
+        arguments=['-d{}'.format(rviz_config)],
+        parameters=[{
+            'robot_description': robot_description,
+            'robot_description_semantic': robot_description_semantic,
+            }],
     )
 
+    return [robot_state_publisher, joint_state_publisher_gui, rviz]
+
+
+def generate_launch_description():
     return LaunchDescription([
-        robot_state_publisher,
-        joint_state_publisher_gui,
-        rviz,
-        ]
-    )
+        DeclareLaunchArgument('rviz_config', default_value=[os.path.join(get_package_share_directory('snp_support'), 'config', 'rviz_base_config.rviz')]),
+        OpaqueFunction(function=launch_setup)
+        ])
