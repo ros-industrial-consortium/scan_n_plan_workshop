0b8bf072a8fba3c711792706a9d014db5f4bf5cc
Michael Ripperger <michael.ripperger@swri.org>
2022-05-03 17:10:09 -0500

    Added signal handler to quit Qt application

--- snp_application/src/main.cpp
+++ snp_application/src/main.cpp
@@ -3,20 +3,34 @@
 
 #include <rclcpp/rclcpp.hpp>
 
+void handleSignal(int)
+{
+  QApplication::instance()->quit();
+}
+
 int main(int argc, char* argv[])
 {
   rclcpp::init(argc, argv);
 
-  QApplication a(argc, argv);
+  QApplication app(argc, argv);
+
+  // Handle signals to terminate the Qt Application
+  signal(SIGINT, handleSignal);
+  signal(SIGTERM, handleSignal);
+
   ROSConWindow w;
   w.show();
 
-  std::thread t{ [&w]() {
-    rclcpp::spin(w.getNode());
-    rclcpp::shutdown();
-  } };
+  // Move the ROS spinning into a separate thread since the call to `spin` is synchronous
+  std::thread t{ [&w]() { rclcpp::spin(w.getNode()); } };
 
-  auto ret = a.exec();
+  // Run the Qt application, which is also sychronous
+  auto ret = app.exec();
+
+  // Shut down ROS to terminate call to `spin`
+  rclcpp::shutdown();
+
+  // Wait for the thread to finish
   t.join();
 
   return ret;
