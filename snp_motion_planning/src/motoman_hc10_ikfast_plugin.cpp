7065c2c9af3927c563747732ac75dd2be86c25c0
Michael Ripperger <michael.ripperger@swri.org>
2022-05-16 18:09:17 -0500

    Update to 0.7.4 version of IKFast constructor; add code for extracting redundancy capable joints

--- snp_motion_planning/src/motoman_hc10_ikfast_plugin.cpp
+++ snp_motion_planning/src/motoman_hc10_ikfast_plugin.cpp
@@ -54,30 +54,66 @@ public:
   {
     auto base_link = get<std::string>(config, "base_link");
     auto tip_link = get<std::string>(config, "tip_link");
-    std::size_t n_joints = get<std::size_t>(config, "n_joints");
+    auto n_joints = get<std::size_t>(config, "n_joints");
 
     // Get the free joint states
-    std::vector<std::vector<double>> free_joint_states;
+    std::map<std::size_t, std::vector<double>> free_joint_states_map;
+    try
     {
       const YAML::Node& free_joint_states_node = config["free_joint_states"];
-      for (const YAML::Node& n : free_joint_states_node)
+      for (auto it = free_joint_states_node.begin(); it != free_joint_states_node.end(); ++it)
       {
-        auto states = n.as<std::vector<double>>();
-        free_joint_states.insert(free_joint_states.begin(), states);
+        free_joint_states_map[it->first.as<std::size_t>()] = it->second.as<std::vector<double>>();
       }
+    }
+    catch (const std::exception&)
+    {
+      CONSOLE_BRIDGE_logDebug("No free joint states specified for IKFast plugin");
     }
 
     // Get the active joints in between the base link and tip link
     tesseract_scene_graph::ShortestPath path = scene_graph.getShortestPath(base_link, tip_link);
-    if (path.active_joints.size() != n_joints)
+
+    // Check the joints specification
+    if (path.active_joints.size() != n_joints + free_joint_states_map.size())
     {
       std::stringstream ss;
-      ss << "Kinematic chain between '" << base_link << "' and '" << tip_link << "' contains "
-         << path.active_joints.size() << " joints, but only " << n_joints << " can be used";
+      ss << "Number of active joints (" << path.active_joints.size()
+         << ") must equal the sum of the number of nominal joints (" << n_joints << ") and the number of free joints ("
+         << free_joint_states_map.size() << ")";
       throw std::runtime_error(ss.str());
     }
 
-    return std::make_unique<IKFastInvKin>(base_link, tip_link, path.active_joints, solver_name, free_joint_states);
+    // Get the redundancy capable joint indices
+    std::vector<Eigen::Index> redundancy_capable_joint_indices;
+    try
+    {
+      redundancy_capable_joint_indices = config["redundancy_capable_joint_indices"].as<std::vector<Eigen::Index>>();
+    }
+    catch (const std::exception&)
+    {
+      CONSOLE_BRIDGE_logDebug("Using default redundancy capable joints for IKFast plugin");
+
+      for (std::size_t i = 0; i < path.active_joints.size(); ++i)
+      {
+        if(free_joint_states_map.find(i) == free_joint_states_map.end())
+        {
+          const auto& joint = scene_graph.getJoint(path.active_joints.at(i));
+          if (joint->limits->upper - joint->limits->lower > 2 * M_PI)
+          {
+            redundancy_capable_joint_indices.push_back(static_cast<Eigen::Index>(i));
+          }
+        }
+      }
+    }
+
+    std::vector<std::vector<double>> free_joint_states;
+    free_joint_states.reserve(free_joint_states_map.size());
+    std::transform(free_joint_states_map.begin(), free_joint_states_map.end(), std::back_inserter(free_joint_states),
+                   [](const std::pair<const std::size_t, std::vector<double>>& pair) { return pair.second; });
+
+    return std::make_unique<IKFastInvKin>(base_link, tip_link, path.active_joints, redundancy_capable_joint_indices,
+                                          solver_name, free_joint_states);
   }
 };
 
