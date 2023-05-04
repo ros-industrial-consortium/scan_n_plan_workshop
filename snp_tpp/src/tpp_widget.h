871565628729fa818577b7954f4000042577c924
Michael Ripperger <michael.ripperger@swri.org>
2023-05-04 16:16:51 -0500

    Updated SNP TPP widget to load configuration from file specified as a parameter of the node

--- snp_tpp/src/tpp_widget.h
+++ snp_tpp/src/tpp_widget.h
@@ -10,6 +10,11 @@ namespace noether
 class TPPPipelineWidget;
 }
 
+namespace Ui
+{
+class TPPWidget;
+}
+
 namespace snp_tpp
 {
 class TPPWidget : public QWidget
@@ -23,8 +28,13 @@ private:
   void callback(const snp_msgs::srv::GenerateToolPaths::Request::SharedPtr req,
                 const snp_msgs::srv::GenerateToolPaths::Response::SharedPtr res);
 
+  void configureTPPPipeline(const std::string& file);
+  void onLoadConfiguration(const bool /*checked*/);
+  void onSaveConfiguration(const bool /*checked*/);
+
   noether::TPPPipelineWidget* pipeline_widget_{ nullptr };
   rclcpp::Service<snp_msgs::srv::GenerateToolPaths>::SharedPtr server_{ nullptr };
+  Ui::TPPWidget* ui_{ nullptr };
 };
 
 }  // namespace snp_tpp
