5937e2840912333dea7d4ff21e36067d232d289a
Michael Ripperger <michael.ripperger@swri.org>
2024-05-10 16:38:31 -0700

    Converted snp_scanning to python package

---
+++ snp_scanning/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+package_name = 'snp_scanning'
+
+setup(
+    name=package_name,
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    scripts=[
+        'scripts/reconstruction_sim_node',
+        'scripts/scan_motion_plan_from_file_node',
+    ],
+)
